ad763ed5cac19046d662b481b6567f87a737387e
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-03-27 16:51:36 -0400

    auth added

--- application/main.py
+++ application/main.py
@@ -4,14 +4,29 @@ The fast api main app
 
 """
 
-from fastapi import FastAPI
+"""Python FastAPI Auth0 integration example
+"""
+
+from fastapi import FastAPI, Depends
 from fastapi_plugin.fast_api_client import Auth0FastAPI
 from application.config import get_settings
 
+
+# Creates app instance
 app = FastAPI()
+
+#print(get_settings().model_dump())
+
+settings = get_settings()
+auth0 = Auth0FastAPI(
+   domain=settings.auth0_domain,
+   audience=settings.auth0_api_audience
+)
+
 
 @app.get("/api/public")
 def public():
+   """No access token required to access this route"""
 
    result = {
        "status": "success",
@@ -19,3 +34,10 @@ def public():
                "authenticated to see this.")
    }
    return result
+
+"""
+@app.get("/api/private")
+def private(claims: dict = Depends(auth0.require_auth())):
+   # A valid access token is required to access this route
+   return claims
+"""
